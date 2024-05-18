a0fe1bc08c33503345dae7a071f08b5a041ee03a
François <25376055+fravan@users.noreply.github.com>
2024-05-18 09:12:22 +0200

    Added simple layout for operators

---
+++ src/lib/components/ProductCell.svelte
@@ -0,0 +1,9 @@
+<script>
+	/** @type {{ product: Product }} */
+	let { product } = $props();
+</script>
+
+<div class="p-2 rounded border">
+	<p>{product.name}</p>
+	<p>{product.price.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' })}</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
 <script>
+	import ProductCell from '$lib/components/ProductCell.svelte';
+	import { createProductsStore } from '$lib/stores/products.svelte.js';
+
+	let store = createProductsStore();
 </script>
 
 <svelte:head>
@@ -6,4 +10,8 @@
 	<meta name="description" content="La Caisse Enregistreuse" />
 </svelte:head>
 
-<section></section>
+<section class="flex flex-col gap-2">
+	{#each store.products as p}
+		<ProductCell product={p} />
+	{/each}
+</section>
