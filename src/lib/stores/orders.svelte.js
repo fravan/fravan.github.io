cdcd0253e847f3fa601016f616054b65ff4c15d2
François <25376055+fravan@users.noreply.github.com>
2024-05-24 08:16:41 +0200

    Only save the recap of all orders to local storage

--- src/lib/stores/orders.svelte.js
+++ src/lib/stores/orders.svelte.js
@@ -1,41 +1,40 @@
-const ordersStoreKey = 'lce:orders:v1';
+const orderStoreKey = 'lce:orders:v1';
 
 export function createOrdersStore() {
-	/** @type Order[] */
-	let orders = $state([]);
-	let encodedOrders = $derived(btoa(encodeOrders(orders)));
+	/** @type Order */
+	let cumulatedOrder = $state({});
+	let encodedOrder = $derived(btoa(encodeOrder(cumulatedOrder)));
 
 	$effect(() => {
-		localStorage.setItem(ordersStoreKey, encodedOrders);
+		localStorage.setItem(orderStoreKey, encodedOrder);
 	});
 
 	return {
-		get orders() {
-			return orders;
+		get order() {
+			return cumulatedOrder;
 		},
 		clear() {
-			orders = [];
+			cumulatedOrder = {};
 		},
 		/**
 		 * @param {Order} order
 		 */
 		saveOrder(order) {
-			const filteredOrder = Object.entries(order).reduce(
-				(/** @type {Order} */ acc, [key, value]) => {
-					if (value.quantity === 0) return acc;
-					acc[key] = value;
-					return acc;
-				},
-				{}
-			);
-			orders.push(filteredOrder);
+			cumulatedOrder = Object.entries(order).reduce((acc, [key, value]) => {
+				if (value.quantity === 0) return acc;
+				if (acc[key] == null) {
+					acc[key] = { quantity: 0, price: value.price };
+				}
+				acc[key].quantity += value.quantity;
+				return acc;
+			}, cumulatedOrder);
 		}
 	};
 }
 
-/** @param {Order[]} orders */
-function encodeOrders(orders) {
-	return orders
-		.map((o) => Object.entries(o).map(([key, value]) => `${key}:${value.quantity}|${value.price}`))
+/** @param {Order} order */
+function encodeOrder(order) {
+	return Object.entries(order)
+		.map(([key, value]) => `${key}:${value.quantity}|${value.price}`)
 		.join(';');
 }
